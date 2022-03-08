c2d199e2e225ef7978b5a5a8114f860ff9c4a1e8
Lisa Harrison <lmharrison0810@gmail.com>
2022-03-07 22:16:05 -0500

    express server basic authentication added

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const partnerRouter = require('./routes/partnerRouter');
 const mongoose = require('mongoose');
 
 const url = 'mongodb://127.0.0.1/nucampsite';
-
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
@@ -37,6 +36,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Authentication middleware
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+  }
+
+  // Buffer is a global class in Node - it can be used without importing any module. from is a static method from Buffer
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+      return next(); // authorized
+  } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
